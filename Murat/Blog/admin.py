0dccfa7e921c3ab6f66dbe5c9107308b364617ae
muratyilmazlab <murat@murat.hacettepe.edu.tr>
2020-05-09 13:12:51 +0300

    murat09052020

--- Murat/Blog/admin.py
+++ Murat/Blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BlogGonderi
 
+admin.site.register(BlogGonderi)
 # Register your models here.
--- Murat/Blog/views.py
+++ Murat/Blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def gonderi_list(request):
+    return render(request,'Blog/gonderilist.html',{})
+    
 # Create your views here.
