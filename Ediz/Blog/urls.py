54040f802a2ca1607e4dc6c284dc7912a687dd11
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-05-10 12:19:37 +0300

    10052020

--- Ediz/Blog/urls.py
+++ Ediz/Blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.gonderi_list,name="listgonderi"),
     path('detay/<int:pk>/',views.gonderi_detay,name="detaygon"),
-    path('gonderi/yeni/',views.gonderi_duzenle,name="yenigon"),
+    path('gonderi/yeni/',views.gonderi_ekle,name="yenigon"),
+    path('gonderi/duzenle/<int:pk>',views.gonderi_duzenle,name="duzenlegon"),
 ]
--- Ediz/Blog/views.py
+++ Ediz/Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import BlogGonderi
 from .forms import GonderiForm
 
@@ -10,7 +10,7 @@ def gonderi_detay(request,pk):
     gonderi = BlogGonderi.objects.get(pk=pk)
     return render(request,'Blog/gonderidetay.html',{"gonderi":gonderi})
 
-def gonderi_duzenle(request):
+def gonderi_ekle(request):
     if request.method == "POST":
         form = GonderiForm(request.POST)
         if form.is_valid():
@@ -20,4 +20,17 @@ def gonderi_duzenle(request):
             return redirect('detaygon',pk=gonderi.pk)
     else:
         form = GonderiForm()
+    return render(request,'Blog/yenigonderi.html',{"form":form})
+
+def gonderi_duzenle(request,pk):
+    gonderi = get_object_or_404(BlogGonderi,pk=pk)
+    if request.method == "POST":
+        form = GonderiForm(request.POST,instance=gonderi)
+        if form.is_valid():
+            gonderi = form.save(commit=False)
+            # işlemler yapabilirsiniz
+            gonderi.save()
+            return redirect('detaygon',pk=gonderi.pk)
+    else:
+        form = GonderiForm(instance=gonderi)
     return render(request,'Blog/yenigonderi.html',{"form":form})
